43eba58b2807d194d80e8f70cfbe51cbb9fe64cf
Virgil <157654563+Virgildev@users.noreply.github.com>
2024-09-29 18:52:52 -0400

    fix items

--- [install]/items.lua
+++ [install]/items.lua
@@ -1,66 +1,68 @@
-ox_inventory>data>items.lua
+--!!ox_inventory>data>items.lua!!--
 
-['break_cutting_tool'] = {
-    label = 'Break Cutters',
-    description = "Cut them breaks yo",
-    consume = 0,
-    weight = 100,
-    client = {
-        export = 'v-vehiclesab.break_cutting_tool'
-    }
-},
-["tankdrill"] = {
-    label = "Large Bit Drill",
-    weight = 300,
-    stack = true,
-    consume = 1,
-    close = false,
-    description = "The real deal...",
-    client = {
-        export = 'v-vehiclesab.drillTank'
-    }
-},
-["wheelremover"] = {
-    label = "High Power Drill",
-    weight = 300,
-    stack = true,
-    consume = 1,
-    close = false,
-    description = "The real deal...",
-    client = {
-        export = 'v-vehiclesab.wheelRemover'
-    }
-},
-["wheelspanner"] = {
-    label = "Wheel Loosener",
-    weight = 300,
-    stack = true,
-    consume = 1,
-    close = false,
-    description = "The real deal...",
-    client = {
-        export = 'v-vehiclesab.useSpanner'
-    }
-},
-["carbomb"] = {
-    label = "Car Bomb",
-    weight = 500,
-    stack = true,
-    consume = 1,
-    close = false,
-    description = "The real deal...",
-    client = {
-        export = 'v-vehiclesab.bombPlantKit'
-    }
-},
-["gascanister"] = {
-    label = "Gas Canister",
-    weight = 500,
-    stack = true,
-    consume = 1,
-    close = false,
-    description = "The real deal...",
-    client = {
-        export = 'v-vehiclesab.arsonKit'
-    }
-},
+return {
+    ['break_cutting_tool'] = {
+        label = 'Break Cutters',
+        description = "Cut them breaks yo",
+        consume = 0,
+        weight = 100,
+        client = {
+            export = 'v-VehicleSabotage.break_cutting_tool'
+        }
+    },
+    ["tankdrill"] = {
+        label = "Large Bit Drill",
+        weight = 300,
+        stack = true,
+        consume = 1,
+        close = false,
+        description = "The real deal...",
+        client = {
+            export = 'v-VehicleSabotage.drillTank'
+        }
+    },
+    ["wheelremover"] = {
+        label = "High Power Drill",
+        weight = 300,
+        stack = true,
+        consume = 1,
+        close = false,
+        description = "The real deal...",
+        client = {
+            export = 'v-VehicleSabotage.wheelRemover'
+        }
+    },
+    ["wheelspanner"] = {
+        label = "Wheel Loosener",
+        weight = 300,
+        stack = true,
+        consume = 1,
+        close = false,
+        description = "The real deal...",
+        client = {
+            export = 'v-VehicleSabotage.useSpanner'
+        }
+    },
+    ["carbomb"] = {
+        label = "Car Bomb",
+        weight = 500,
+        stack = true,
+        consume = 1,
+        close = false,
+        description = "The real deal...",
+        client = {
+            export = 'v-VehicleSabotage.bombPlantKit'
+        }
+    },
+    ["gascanister"] = {
+        label = "Gas Canister",
+        weight = 500,
+        stack = true,
+        consume = 1,
+        close = false,
+        description = "The real deal...",
+        client = {
+            export = 'v-VehicleSabotage.arsonKit'
+        }
+    },
+}
